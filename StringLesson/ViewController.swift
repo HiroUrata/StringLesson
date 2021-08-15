7f0433e81a8339a60e2076bb8d07e9f8eaf2fb3e
HiroUrata <1712026@i.softbank.jp>
2021-08-15 23:14:30 +0900

    Initial Commit

---
+++ StringLesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StringLesson
+//
+//  Created by UrataHiroki on 2021/08/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
