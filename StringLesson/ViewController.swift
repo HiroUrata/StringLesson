4b1b8318e7dfc6a93707867cb5d19acfcec68849
HiroUrata <1712026@i.softbank.jp>
2021-08-15 23:49:45 +0900

    replacingOccurrences(of: "1", with: "2") 追加

--- StringLesson/ViewController.swift
+++ StringLesson/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     var uppercasedString = "abcdefg"
     var lowercasedString = "ABCDEFG"
     var containString = "0123456789"
-    
+    var replacingOccurrencesString = "1111111"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         print(uppercasedString.uppercased())
         print(lowercasedString.lowercased())
         print(containString.contains("0"))
-    
+        print(replacingOccurrencesString.replacingOccurrences(of: "1", with: "2"))
         
     }
 
